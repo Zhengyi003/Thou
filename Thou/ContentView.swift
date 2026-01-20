8b75deb02c886dc9545a382192e76da5a2ed6ce6
李正一 <641762500@qq.com>
2026-01-20 20:57:18 +0800

    Initial Commit

---
+++ Thou/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Thou
+//
+//  Created by 李正一 on 2026/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Thou/ThouApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThouApp.swift
+//  Thou
+//
+//  Created by 李正一 on 2026/1/20.
+//
+
+import SwiftUI
+
+@main
+struct ThouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
